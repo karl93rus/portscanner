c817bcd457c95efc769bc36a606c1cde615e177e
Alex Sazonov <karl93rus@gmail.com>
2019-09-23 21:55:08 +0300

    scans. TODO: concurrent scan

--- main.go
+++ main.go
@@ -2,27 +2,75 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 )
+
+type Host struct {
+	host    string
+	minPort int
+	curPort int
+	maxPort int
+	lock    sync.Mutex
+}
+
+func (h *Host) scan() {
+	fmt.Printf("In method: %v\n", h)
+	for h.curPort < h.maxPort {
+		h.lock.Lock()
+		hostPort := h.host + ":" + strconv.Itoa(h.curPort)
+		fmt.Println("In method. Host: ", hostPort)
+		conn, err := net.DialTimeout("tcp", hostPort, time.Millisecond*500)
+		if err != nil {
+			continue
+		} else {
+			fmt.Println("TCP port open: " + strconv.Itoa(h.curPort))
+			continue
+		}
+		conn.Close()
+		h.curPort++
+		h.lock.Unlock()
+	}
+	wg.Done()
+}
+
+var wg sync.WaitGroup
 
 func main() {
 	host := os.Args[1]
-	// port := os.Args[2]
 
-	for i := 1; i < 65535; i++ {
+	h := &Host{
+		host:    host,
+		minPort: 1,
+		curPort: 1,
+		maxPort: 10000,
+	}
+
+	fmt.Printf("In main. Host: %v\n", h.host)
+
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go h.scan()
+	}
+	wg.Wait()
+
+	// scanPorts(host)
+}
+
+func scanPorts(host string) {
+	for i := 77; i < 10000; i++ {
 		hostPort := host + ":" + strconv.Itoa(i)
 		fmt.Println(hostPort)
-		conn, err := net.Dial("tcp", hostPort)
+		conn, err := net.DialTimeout("tcp", hostPort, time.Millisecond*500)
 		if err != nil {
 			continue
 		} else {
 			fmt.Println("TCP port open: " + strconv.Itoa(i))
 			continue
 		}
-		defer conn.Close()
-		io.Copy(os.Stdin, conn)
+		conn.Close()
 	}
 }
